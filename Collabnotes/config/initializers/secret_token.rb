0d39703a462b087f3275743ccd2dad588a57ecf1
Dennis Hodapp <thehodapp@gmail.com>
2013-11-15 22:54:16 -0600

    Added Rails directory

---
+++ Collabnotes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collabnotes::Application.config.secret_key_base = 'c7aa37fdd99c59ee5f97d5becea86f99a529f52daee5cad15e136bd670f8e184659e547de092733393e76e74761cdf62ea0fc33a97190e6752b30f8edf79a7b6'
---
+++ Collabnotes/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Collabnotes::Application.config.session_store :cookie_store, key: '_Collabnotes_session'
