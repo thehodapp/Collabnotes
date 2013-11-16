ed6b9a7e65acda92e8196531c7b0990ad728712f
Dennis Hodapp <thehodapp@gmail.com>
2013-11-16 01:08:03 -0600

    Added validations and model stuff to note and user

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
 class Note < ActiveRecord::Base
-  belongs_to :owner
+
+  belongs_to :owner, class_name: 'User'
+
+  validates :title, presence: true
+  validates :upvotes, length: { minimum: 0 }
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+  has_many :notes
+  
+  before_save { self.email.downcase! }
+  before_save { self.username.downcase! }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true
+  validates :password, length: { minimum: 8 }
+  validates :username, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
 end
